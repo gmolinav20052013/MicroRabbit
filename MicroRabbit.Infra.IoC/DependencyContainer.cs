f7eac07991ffb8644cd99d25c6e7520c35f2b8fe
Guillermo Molina <gmolinav2005@gmail.com>
2025-09-05 19:22:35 -0600

    Initial Commit of Dependency Injection

---
+++ MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+            // Here you can register your services, repositories, and other dependencies
+            // For example:
+            // services.AddTransient<IYourService, YourServiceImplementation>();
+        }
+    }
+}
